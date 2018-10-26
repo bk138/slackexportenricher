e9df3c25e0a57d4a8c7ab31b8411744616338ace
Christian Beier <dontmind@freeshell.org>
2018-10-27 00:17:49 +0200

    Initial commit
    Almost working with a limited dataset.
    
    TODO: multiline texts

---
+++ slackexportenricher.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# unpack
+#unzip "$1"
+
+rm -rf out
+mkdir out
+
+# get channel json files
+for J in $(find . -mindepth 2 -name \*.json)
+do
+	echo "processing $J"
+
+	mkdir -p out/$(dirname $J)
+
+	echo "[" >> out/$J
+
+	COUNT=$(jshon -l < $J)
+	I=0
+	while [ $I -lt $COUNT ]
+	do
+
+	    MSG="$(jshon -e $I < $J)"
+
+	    TEXT=$(echo $MSG | jshon -e text)
+
+	    if [ "$TEXT" = '""' ]
+	    then
+		echo "  empty"
+		if echo $(echo $MSG | jshon -k) | grep -q files
+		then
+		    echo "   has files"
+		    FILE_URLS="$(echo $MSG | jshon -e files -a -e url_private)"
+		    MSG=$(echo $MSG | jshon -s $FILE_URLS -i text)
+		fi
+	    fi
+
+	    echo $MSG >> out/$J
+
+	    if [ $I -lt $(($COUNT - 1)) ]
+	    then
+	    	echo "," >> out/$J
+	    fi
+
+	    # go on
+	    I=$(($I + 1))
+	done 
+
+	echo "]" >> out/$J
+done
